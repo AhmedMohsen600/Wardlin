5b2d2775f3e0d44cc3e07f1886315773244fde17
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2023-11-29 19:42:23 +0200

    style google button

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,13 +1,32 @@
 import { signInWithPopup } from 'firebase/auth';
+import { Button, styled } from '@mui/material';
+
 import { auth, provider } from '../firebase/firebase';
+const SignInContainer = styled('div')({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+});
+
+const SignInButton = styled(Button)({
+  padding: '10px 20px',
+  fontSize: '16px',
+});
 
 function SignIn() {
   const signInWithGoogle = () => signInWithPopup(auth, provider);
 
   return (
-    <div>
-      <button onClick={signInWithGoogle}>Sign In With Google</button>
-    </div>
+    <SignInContainer>
+      <SignInButton
+        variant='contained'
+        color='primary'
+        onClick={signInWithGoogle}
+      >
+        Sign In With Google
+      </SignInButton>
+    </SignInContainer>
   );
 }
 
